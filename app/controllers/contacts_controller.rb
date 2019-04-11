19c37b89154cb0f3c69d0069ca4445ee49719b0e
Rocio Pantalone <rocio.pantalone@gmail.com>
2019-04-11 22:50:06 +0000

    Errors solved

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -21,7 +21,6 @@ class ContactsController < ApplicationController
     private
         def contact_params
             params.require(:contact).permit(:name, :email, :comments)
+            params.inspect
         end
 end
-
-params.inspect
