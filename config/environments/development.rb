46dab7bb2d0d887858584cb3265b68b74df8efd8
Rocio Pantalone <rocio.pantalone@gmail.com>
2019-04-11 22:17:11 +0000

    Errors fixed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,3 +52,13 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['587'],
+  :address        => ENV['smtp.gmail.com'],
+  :user_name      => ENV['DevMatchRocio@gmail.com'],
+  :password       => ENV['Arbolcaido2020.'],
+  :domain         => 'protected-refuge-99929.herokuapp.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,3 +84,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['587'],
+  :address        => ENV['smtp.gmail.com'],
+  :user_name      => ENV['DevMatchRocio@gmail.com'],
+  :password       => ENV['Arbolcaido2020.'],
+  :domain         => 'protected-refuge-99929.herokuapp.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
