e6bd728cec28130fa15f0f4deee5f86c8ada5eaf
Rocio Pantalone <rocio.pantalone@gmail.com>
2019-04-11 20:49:15 +0000

    Email changes

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,18 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address: 'smtp.gmail.com',
+
+config.action_mailer.default_url_options = { :host => 'protected-refuge-99929.herokuapp.com' }  
+config.action_mailer.delivery_method = :smtp  
+config.action_mailer.perform_deliveries = true  
+config.action_mailer.raise_delivery_errors = false  
+config.action_mailer.default :charset => "utf-8"  
+config.action_mailer.smtp_settings = {  
+  address: "smtp.gmail.com",
   port: 587,
-  user_name: 'DevMatchRocio@gmail.com',
-  password: 'Arbolcaido2020.',
-  domain: 'protected-refuge-99929.herokuapp.com',
-  authentication: 'plain',
-  enable_starttls_auto: true
+  domain: "protected-refuge-99929.herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["DevMatchRocio@gmail.com"],
+  password: ENV["Arbolcaido2020."]
 }
